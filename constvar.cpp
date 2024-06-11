4dd54a70ec4eb197391cd7ec1f773dd714e6ab25
jeshi2 <jamesvean19@gmail.com>
2024-06-11 11:36:20 +0300

    const

---
+++ constvar.cpp
@@ -0,0 +1,12 @@
+ #include<iostream>
+ #include<string>
+using std::cout;
+using std::endl;
+using std::cin;
+ int main()
+ {
+    const int a=89;
+    cout<<a<<endl;
+    const std::string str="jeshi";
+    cout<<str<<endl;
+ }
