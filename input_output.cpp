2ad91ed60aa25eab1489622394b97a8b77460c2c
jeshi2 <jamesveans19@gmail.com>
2024-07-12 11:41:48 +0300

    iostream

---
+++ input_output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[50];
+    int age;
+    cout<<"enter name:"<<endl;
+    cin>>name;
+    cout<<"enter age:"<<endl;
+    cin>>age;
+
+    cout<<"The name is "<<name<<" and age is "<<age<<endl;
+    return 0;
+}
