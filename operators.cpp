fc3f8c6be0f0661bcb09426bf80ed8fc1d633c9c
jeshi2 <jamesvean19@gmail.com>
2024-08-03 20:31:10 +0300

    operator1

---
+++ operators.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Arithmetic Operators
+    /**int x=10,y=3;
+    cout<<"additon:"<<x+y<<endl;
+    cout<<"Subtraction:"<<x-y<<endl;
+    cout<<"Multiplication:"<<x*y<<endl;
+    cout<<"Division:"<<x/y<<endl;
+    cout<<"Modulo:"<<x%y<<endl;**/
+
+    //post increment and preincrement
+    /**int x=10;
+    int y;
+    y=++x;
+    cout<<x<<endl<<y<<endl;
+    y=x++;
+    cout<<x<<endl<<y<<endl;**/
+
+    //relational operators
+    int x=3,y=6;
+    cout<<"x<y:"<<(x<y)<<endl;
+    cout<<"x>y:"<<(x>y)<<endl;
+    cout<<"x<=y:"<<(x<=y)<<endl;
+    cout<<"x>=y:"<<(x>=y)<<endl;
+    cout<<"x==y:"<<(x==y)<<endl;
+    cout<<"x!=y:"<<(x!=y)<<endl;
+}
