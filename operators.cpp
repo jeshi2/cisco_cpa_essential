821d0d759c7c848e674078f20df43a8267d7e7a0
jeshi2 <jamesvean19@gmail.com>
2024-08-03 21:01:41 +0300

    operator2

--- operators.cpp
+++ operators.cpp
@@ -19,11 +19,39 @@ int main()
     cout<<x<<endl<<y<<endl;**/
 
     //relational operators
-    int x=3,y=6;
+    /**int x=3,y=6;
     cout<<"x<y:"<<(x<y)<<endl;
     cout<<"x>y:"<<(x>y)<<endl;
     cout<<"x<=y:"<<(x<=y)<<endl;
     cout<<"x>=y:"<<(x>=y)<<endl;
     cout<<"x==y:"<<(x==y)<<endl;
-    cout<<"x!=y:"<<(x!=y)<<endl;
+    cout<<"x!=y:"<<(x!=y)<<endl;**/
+
+    //Logical operator
+    /*int x=5, y=10;
+    if((x>5)&&(y<15)){
+        cout<<"x+y:"<<(x+y)<<endl;
+    }
+    else if((x<=5) || (y>15))
+    {
+        cout<<"x-y:"<<(x-y)<<endl;
+    }
+    else{
+        cout<<"!x"<<!x<<endl;
+    }*/
+
+   //bitwise operators
+   int x=26, y=9;
+   cout<<"x&y:"<<(x&y)<<endl;
+   cout<<"x|y:"<<(x|y)<<endl;
+   cout<<"x^y:"<<(x^y)<<endl;
+   cout<<"x<<y:"<<(x<<y)<<endl;
+   cout<<"x>>y:"<<(x>>y)<<endl;
+   cout<<"~x:"<<(~x)<<endl;
+
+
+   //Miscellanous operator
+   /**float x=6.78;
+   cout<<(int)x<<endl;
+   cout<<x<<endl;**/
 }
