b469c3d4e209401abade5a72cf261aaa46f09557
jeshi2 <jamesvean19@gmail.com>
2024-08-17 20:00:34 +0300

    loop

--- for_loop.cpp
+++ for_loop.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main()
 {
-    for(int i=1; i<=10; i++)
+    int i=1,j;
+    for(i=1, j=0; i<=5, j<3; ++i, j++)
     {
-        cout<<i<<endl;
+        cout<<"i="<<i<<" "<<"j="<<j<<endl;
     }
+    cout<<"end of loop";
     return 0;
 }
