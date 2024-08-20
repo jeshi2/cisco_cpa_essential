47b14ed59580a76a20a4a6525aa7aedb390f7546
jeshi2 <jamesvean19@gmail.com>
2024-08-20 20:36:46 +0300

    while

---
+++ whileloop2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int i;
+    cout<<"enter i: ";
+    cin>>i;
+    while(1)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    cout<<"out of loop";
+    return 0;
+}
