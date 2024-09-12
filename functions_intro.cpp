31a4169161553425e6932781ca52470ad60d24d0
jeshi2 <jamesvean19@gmail.com>
2024-09-12 20:33:21 +0300

    function intr

---
+++ functions_intro.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+// int sum(int x, int y); // function declaration/prototypes
+// string display();
+void greet()
+{
+    cout<<"Hi"<<endl;
+}
+void display()
+{
+    cout<<"I am in a display"<<endl;
+    greet();
+    cout<<"I am exiting display function";
+}
+int main()
+{
+    // sum(3,4);
+    //  string s;
+    //  s=display();
+    //  cout<<display();
+    display();
+    return 0;
+}
+
+// string display()
+// {
+//     string name = "Luke";
+//     return name;
+// }
+
+// void display(string name)
+// {
+//     cout<<"Hi "<<name;
+// }
+
+// int sum(int a, int b)
+// {
+//     // int a, b, sum = 0;
+//     // cout << "enter a and b values: ";
+//     // cin >> a >> b;
+//     // sum = a + b;
+//     // cout << sum;
+//     return a+b; //optional
+// }
